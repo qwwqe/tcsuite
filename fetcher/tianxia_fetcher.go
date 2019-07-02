bf5622a8682de9a6a9012bd5c8ec498e5821bcfc
qwwqe <f.werth@gmail.com>
2019-07-02 18:17:29 +0800

    Add tianxia fetcher stub

---
+++ fetcher/tianxia_fetcher.go
@@ -0,0 +1,33 @@
+package fetcher
+
+// json article api format: https://api-app.cw.com.tw/cw-app/article/5002959
+// json format:
+// {
+// 	'success':'ok',
+// 	'code':'0000',
+// 	'msg':'loading success',
+// 	'items':[
+// 		{
+// 			'id': string,
+// 			'channel': string,
+// 			'magazine_period': ,
+// 			'title': string,
+// 			'attribute': ,
+// 			'author': string,
+// 			'author_writer_type': string,
+// 			'publish_time': string,
+// 			'share_url': string,
+// 			'preface': string,
+// 			'article_reference': ,
+// 			'content': string,
+// 			'keywords': []string,
+// 			'external_articles': [],
+// 			'related_articles': [
+// 				{
+// 					'id': ,
+// 					'title': ,
+// 				}
+// 			],
+// 		}
+// 	]
+// }
