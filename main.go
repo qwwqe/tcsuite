b5d198121fe746b627276b0cbb99067bd9fc07d6
qwwqe <f.werth@gmail.com>
2019-07-09 21:34:39 +0800

    Add command line options

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"golang.org/x/text/language"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	//"github.com/qwwqe/tcsuite/content"
 	f "github.com/qwwqe/tcsuite/fetcher"
+	l "github.com/qwwqe/tcsuite/lexicon"
 	r "github.com/qwwqe/tcsuite/repository"
 )
 
@@ -44,30 +51,95 @@ var fetchOptionSets = []FetchOptionSet{
 	},
 }
 
+var usage = "Usage: tcsuite <fetch | poplex> <lexicon file>\n"
+
 func main() {
+	if len(os.Args) == 1 {
+		fmt.Printf(usage)
+		os.Exit(0)
+	}
+
 	repo := r.GetRepository(r.RepositoryOptions{
 		RestoreRequestHistory: false,
 	})
 
-	for _, fOpts := range fetchOptionSets {
-		fOpts.Fetcher.SetFetcherOptions(&f.FetcherOptions{
-			Repository: repo,
-		})
-	}
-
-	mode := "update"
-
-	for _, fOpts := range fetchOptionSets {
-		var fetchOpts f.FetchOptions
-		switch mode {
-		case "update":
-			fetchOpts = fOpts.UpdateSet
-		case "initial":
-			fetchOpts = fOpts.InitialSet
-		default:
-			continue
+	switch os.Args[1] {
+	case "fetch":
+		for _, fOpts := range fetchOptionSets {
+			fOpts.Fetcher.SetFetcherOptions(&f.FetcherOptions{
+				Repository: repo,
+			})
 		}
-		fOpts.Fetcher.Fetch(fetchOpts)
-	}
 
+		mode := "update"
+
+		for _, fOpts := range fetchOptionSets {
+			var fetchOpts f.FetchOptions
+			switch mode {
+			case "update":
+				fetchOpts = fOpts.UpdateSet
+			case "initial":
+				fetchOpts = fOpts.InitialSet
+			default:
+				continue
+			}
+			fOpts.Fetcher.Fetch(fetchOpts)
+		}
+	case "poplex":
+		if len(os.Args) < 3 {
+			fmt.Printf(usage)
+			os.Exit(1)
+		}
+
+		lexiconName := "Traditional Chinese Comprehensive"
+		lexiconLang := language.MustParse("zh-tw").String()
+		lexicon := l.NewLexicon(lexiconName, lexiconLang)
+
+		err := lexicon.LoadRepository(repo)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if lexicon.NumEntries() == 0 {
+			file, err := os.Open(os.Args[2])
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			defer file.Close()
+
+			lexemes := make([]string, 0)
+			frequencies := make([]int, 0)
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				text := strings.TrimSpace(scanner.Text())
+				if strings.HasPrefix("#", text) {
+					continue
+				}
+
+				fields := strings.Fields(text)
+				if len(fields) != 2 {
+					continue
+				}
+				freq, err := strconv.Atoi(fields[1])
+				if err != nil {
+					continue
+				}
+
+				lexemes = append(lexemes, fields[0])
+				frequencies = append(frequencies, freq)
+			}
+			err = lexicon.AddLexemes(lexemes, frequencies)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+
+		fmt.Printf("Lexicon \"%s\" has %d entries.\n", lexiconName, lexicon.NumEntries())
+	default:
+		fmt.Printf(usage)
+		os.Exit(1)
+	}
 }
