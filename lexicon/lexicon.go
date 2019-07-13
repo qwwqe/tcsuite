69db0e099375623d2db79c1567e2d8ac43d46ae9
qwwqe <f.werth@gmail.com>
2019-07-14 02:10:56 +0800

    Tweak go doc comment

--- lexicon/lexicon.go
+++ lexicon/lexicon.go
@@ -8,7 +8,7 @@ type Lexicon interface {
 	AddLexeme(lexeme string, frequency int) error
 	AddLexemes(lexemes []string, frequencies []int) error
 	GetLexemeFrequency(lexeme string) (frequency int, isPrefix bool, exists bool)
-	// Register a repository with the lexicon.
+	// LoadRepository registers a repository with the lexicon.
 	// Implementers should prepare any temporary data structures they need in this function.
 	LoadRepository(repo repository.Repository) error
 	NumEntries() int
